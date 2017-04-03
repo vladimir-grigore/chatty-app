145707d9a6945a1bad056823e682c6c3073da6e6
Vladimir Grigore <vladimir.grigore@gmail.com>
2017-04-03 23:26:30 +0000

    Update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,7 @@ var webpack = require('webpack');
 
 module.exports = {
   devtool: 'eval',
-  entry: [
-    'webpack-dev-server/client?http://localhost:3000',
-    './src/index.jsx'
-  ],
+  entry: './src/index.jsx',
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',

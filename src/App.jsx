fa851e045de62dc03c022ea41b3e75ac83223050
Vladimir Grigore <vladimir.grigore@gmail.com>
2017-04-04 01:13:58 +0000

    Add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,10 @@ import ChatBar from './ChatBar.jsx';
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Chatty</h1>
+      <main class="messages">
         <Message/>
         <ChatBar/>
-      </div>
+      </main>
     );
   }
 }

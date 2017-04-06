2aa85e2472fea02a26b56a7bfa49255f1ba84ffc
Vladimir Grigore <vladimir.grigore@gmail.com>
2017-04-06 23:54:38 +0000

    Manage image files

--- src/Message.jsx
+++ src/Message.jsx
@@ -1,5 +1,21 @@
 import React, {Component} from 'react';
 
+function CustomContent(props) {
+  let imageChecker = /([a-z\-_0-9\/\:\.]*\.jpg|jpeg|png|gif)/i;
+  // console.log("props.content:", props.content)
+  // console.log("CHECKER_exec:", imageChecker.test(props.content));
+
+  if(imageChecker.test(props.content)){
+    return(
+      <img src={props.content}/>
+    )
+  } else {
+    return(
+      <div>{props.content}</div>
+    )
+  }
+}
+
 class Message extends Component {
   static get propTypes() { 
     return { 
@@ -18,7 +34,7 @@ class Message extends Component {
     return (
       <div className='message'>
         <span className='message-username' style={this.props.style}>{this.props.username}</span>
-        <span className='message-content'>{this.props.content}</span>
+        <span className='message-content'><CustomContent content={this.props.content}/></span>
       </div>
     );
   }
